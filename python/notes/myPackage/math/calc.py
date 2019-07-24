f534960577a7c0d8267d0781af8c1ba1474c6b7d
banggeut01 <genie121110@gmail.com>
2019-07-24 17:42:08 +0900

    190724 | python 예외처리, OOT

---
+++ python/notes/myPackage/math/calc.py
@@ -0,0 +1,23 @@
+def sum(a, b):
+    '''
+    두 수를 더합니다.
+    '''
+    return a + b
+
+def sub(a, b):
+    '''
+    두 수를 빼줍니다.
+    '''
+    return a - b
+
+def mul(a, b):
+    '''
+    두 수를 곱합니다.
+    '''
+    return a * b
+
+def div(a, b):
+    '''
+    두 수를 나눕니다.
+    '''
+    return a / b
